dac17ff4ed4af0ea8f75d2bdbbce0d21e5a0f8f5
w22543 <w22543@w22543deiMac.local>
2017-04-06 10:52:07 +0800

    Initial Commit

---
+++ WPushDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WPushDemo
+//
+//  Created by w22543 on 2017/4/6.
+//  Copyright © 2017年 wd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WPushDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WPushDemo
+//
+//  Created by w22543 on 2017/4/6.
+//  Copyright © 2017年 wd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
